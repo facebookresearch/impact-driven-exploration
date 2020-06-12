836586d73b7709350cc8d52ecaa7fbf042b09fb1
rraileanu <raileanu.roberta@gmail.com>
2020-06-12 11:16:37 -0400

    Fixed typo

--- src/algos/curiosity.py
+++ src/algos/curiosity.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-simport logging
+import logging
 import os
 import sys
 import threading
